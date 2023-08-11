66e2c3fe7cc07e26bc5cd35051dbde429cd59221
ItVal <valentinhonvj1@gmail.com>
2023-08-11 11:30:26 +0200

    feat implementation signup service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { SignupDto } from './dto/signupDto';
+import { from } from 'rxjs';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
-  signup(signupDto: SignupDto) {
+  async signup(signupDto: SignupDto) {
     // vérifier si l'utilisateur est déjà inscrit
-
+    const { email, password, username } = signupDto;
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (user) throw new ConflictException('User already exists');
     // hasher le mot de passe
-
+    const hash = await bcrypt.hash(password, 10);
     //Enregistrer l'utilisateur dans la base de données
-
+    await this.prismaService.user.create({
+      data: { username, email, password: hash },
+    });
     // Envoyer un email de confirmation
 
     // retourner une reponse de succès
-    return 'signup';
+    return { data: 'User succesfully created' };
   }
 
   login() {
