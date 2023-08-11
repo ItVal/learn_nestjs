56d1ca2868773d49d959d7c2fa2d3f50910d2a47
ItVal <valentinhonvj1@gmail.com>
2023-08-11 12:36:36 +0200

    feat implemention send mail service

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
-
+import * as nodemailer from 'nodemailer';
 @Injectable()
-export class MailerService {}
+export class MailerService {
+  //config nodemailer
+  private async transporter() {
+    const testaccount = await nodemailer.createTestAccount();
+    const transport = nodemailer.createTransport({
+      host: 'localhost',
+      port: 1025,
+      ignoreTLS: true,
+      auth: {
+        user: testaccount.user,
+        pass: testaccount.pass,
+      },
+    });
+    return transport;
+  }
+  // send mail function
+  async sendSignupEmilConfirmation(userEmail: string) {
+    (await this.transporter()).sendMail({
+      from: 'irnas@localhost.com',
+      to: userEmail,
+      subject: 'Inscription',
+      html: '<h3>Your account is successfuly created',
+    });
+  }
+}
