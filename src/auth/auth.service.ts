11839147627cb1e6dbc880ab19c17a97af4c7dca
ItVal <valentinhonvj1@gmail.com>
2023-08-11 11:01:30 +0200

    feat config prisma service and environment variable

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { SignupDto } from './dto/signupDto';
+
 @Injectable()
 export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
   signup(signupDto: SignupDto) {
+    // vérifier si l'utilisateur est déjà inscrit
+
+    // hasher le mot de passe
+
+    //Enregistrer l'utilisateur dans la base de données
+
+    // Envoyer un email de confirmation
+
+    // retourner une reponse de succès
     return 'signup';
   }
 
