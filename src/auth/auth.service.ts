14443b23e52aa78c1acfb8dc9cc3f4f341051045
ItVal <valentinhonvj1@gmail.com>
2023-08-11 14:57:17 +0200

    feat signin service implementation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,18 +2,22 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { MailerService } from 'src/mailer/mailer.service';
+import { ConfigService } from '@nestjs/config/dist';
+import { JwtService } from '@nestjs/jwt';
 import { SignupDto } from './dto/signupDto';
-import { from } from 'rxjs';
+import { SigninDto } from './dto/signinDto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly prismaService: PrismaService,
     private readonly mailerService: MailerService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
   ) {}
   async signup(signupDto: SignupDto) {
-    // vérifier si l'utilisateur est déjà inscrit
     const { email, password, username } = signupDto;
+    // vérifier si l'utilisateur est déjà inscrit
     const user = await this.prismaService.user.findUnique({
       where: { email },
     });
@@ -30,7 +34,37 @@ export class AuthService {
     return { data: 'User succesfully created' };
   }
 
-  login() {
-    return 'login';
+  // login
+  async signin(signinDto: SigninDto) {
+    const { email, password } = signinDto;
+    //vérifier si l'utilisateur est déjà inscrit
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (!user)
+      throw new ConflictException('User not found or password incorect');
+    //Comparer le mot de passe
+    const match = await bcrypt.compare(password, user.password);
+    if (!match)
+      throw new ConflictException('User not found or password incorect');
+
+    // Retourner un token jwt
+    const payload = {
+      sub: user.userId,
+      email: user.email,
+    };
+
+    const token = this.jwtService.sign(payload, {
+      expiresIn: '1d',
+      secret: this.configService.get('JWT_SECRET_KEY'),
+    });
+
+    return {
+      token,
+      user: {
+        username: user.username,
+        email: user.email,
+      },
+    };
   }
 }
